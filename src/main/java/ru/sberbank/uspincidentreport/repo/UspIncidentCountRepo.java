69e7a82ad5f89c25707797919fc81a508375ca42
eashustov <eashustov@gmail.com>
2022-05-21 19:28:43 +0300

    21.05.22

---
+++ src/main/java/ru/sberbank/uspincidentreport/repo/UspIncidentCountRepo.java
@@ -0,0 +1,125 @@
+package ru.sberbank.uspincidentreport.repo;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ru.sberbank.uspincidentreport.domain.UspIncidentData;
+
+import java.util.List;
+
+@Repository
+public interface UspIncidentCountRepo extends CrudRepository<UspIncidentData, String> {
+
+    //   @Override
+    @Query(value = "SELECT\n" +
+            "\t\"HPC_ASSIGNMENT\", COUNT (\"NUMBER\")AS \"COUNT_INC\"\n" +
+            "FROM\n" +
+            "\t(\tSELECT\n" +
+            "\t\t\tprob1.\"NUMBER\",\n" +
+            "\t\t\tBRIEF_DESCRIPTION,\n" +
+            "\t\t\tPRIORITY_CODE,\n" +
+            "\t\t\tOPEN_TIME,\n" +
+            "\t\t\tto_char(ACTION)                                                           \n" +
+            "\t\t\tAS ACTION,\n" +
+            "\t\t\tREGEXP_SUBSTR(REGEXP_SUBSTR(to_char(ACTION), '^.*(\\s.*){6}.*'), \n" +
+            "\t\t\t'htt.*$') AS ZABBIX_HISTORY,\n" +
+            "\t\tREPLACE\n" +
+            "\t\t\t(\n" +
+            "\t\t\t\tREGEXP_SUBSTR(REGEXP_SUBSTR(to_char(ACTION), '^.*(\\s.*){4}.*'), \n" +
+            "\t\t\t\t'Хост:\\s.*$'),\n" +
+            "\t\t\t\t'Хост: '\n" +
+            "\t\t\t)\n" +
+            "\t\t\tAS HOST,\n" +
+            "\t\t\tREGEXP_SUBSTR(REGEXP_SUBSTR(to_char(ACTION),\n" +
+            "\t'^.*(\\s.*){1}.*'),\n" +
+            "\t'Проблема.*$')                                                              \n" +
+            "\tAS PROBLEM,\n" +
+            "\tHPC_STATUS,\n" +
+            "\tsubstr(prob1.hpc_assignee_name, 1, instr(prob1.hpc_assignee_name, '(') - 2) \n" +
+            "\tAS HPC_ASSIGNEE_NAME,\n" +
+            "\tsubstr(prob1.hpc_assignment, 1, instr(prob1.hpc_assignment, '(') - 2 )      \n" +
+            "\tAS HPC_ASSIGNMENT,\n" +
+            "\tHPC_CREATED_BY_NAME,\n" +
+            "\t'RESOLUTION'                                                                \n" +
+            "\tAS RESOLUTION,\n" +
+            "\tOPENED_BY \n" +
+            "FROM\n" +
+            "\tsmprimary.probsummarym1 prob1 \n" +
+            "WHERE\n" +
+            "\tprob1.hpc_assignment IN ( 'ЦСИТ Серверы приложений (00001092)',\n" +
+            "\t'ЦСИТ ОАСП Стандартные платформы (00003984)',\n" +
+            "\t'Сопровождение WAS ОСЦИТУ ДВБ (00001341)',\n" +
+            "\t'ЦИ Запад Стандартные платформы (00011217)',\n" +
+            "\t'ЦИ Центр Стандартные платформы (00011213)',\n" +
+            "\t'ЦСИТ ОАСП Специализированные платформы (00003978)',\n" +
+            "\t'ЦСИТ ОАСП Интеграционные платформы (00003982)',\n" +
+            "\t'ЦИ ОАСП Системы очередей сообщений (00014339)',\n" +
+            "\t'ЦИ ОАСП Шлюзовые решения (00014345)',\n" +
+            "\t'ЦИ ОАСП Технологический стэк ППРБ (00014341)',\n" +
+            "\t'ЦИ Восток Интеграционные платформы (00011221)',\n" +
+            "\t'ЦИ Запад Интеграционные платформы (00011219)',\n" +
+            "\t'ЦИ Центр Интеграционные платформы (00011215)',\n" +
+            "\t'СБТ ДК ОСА Серверы приложений (Щелчков Р.А.) (00010280)',\n" +
+            "\t'Сопровождение Платформы управления контейнерами (00018435)',\n" +
+            "\t'SberInfra УСП Интеграционные платформы (Гоголев К.Ю.) (00019273)') \n" +
+            "UNION\n" +
+            "SELECT\n" +
+            "\tprob1.\"NUMBER\",\n" +
+            "\tBRIEF_DESCRIPTION,\n" +
+            "\tPRIORITY_CODE,\n" +
+            "\tOPEN_TIME,\n" +
+            "\tto_char(ACTION)                                                           AS \n" +
+            "\tACTION,\n" +
+            "\tREGEXP_SUBSTR(REGEXP_SUBSTR(to_char(ACTION), '^.*(\\s.*){6}.*'), 'htt.*$') AS \n" +
+            "\tZABBIX_HISTORY,\n" +
+            "REPLACE\n" +
+            "\t(\n" +
+            "\t\tREGEXP_SUBSTR(REGEXP_SUBSTR(to_char(ACTION), '^.*(\\s.*){4}.*'), \n" +
+            "\t\t'Хост:\\s.*$'),\n" +
+            "\t\t'Хост: '\n" +
+            "\t)\n" +
+            "\tAS HOST,\n" +
+            "\tREGEXP_SUBSTR(REGEXP_SUBSTR(to_char(ACTION),\n" +
+            "\t'^.*(\\s.*){1}.*'),\n" +
+            "\t'Проблема.*$') AS PROBLEM,\n" +
+            "\tHPC_STATUS,\n" +
+            "\tsubstr(prob1.hpc_assignee_name,\n" +
+            "\t1,\n" +
+            "\tinstr(prob1.hpc_assignee_name,\n" +
+            "\t'(') - 2)      AS HPC_ASSIGNEE_NAME,\n" +
+            "\tsubstr(prob1.hpc_assignment,\n" +
+            "\t1,\n" +
+            "\tinstr(prob1.hpc_assignment,\n" +
+            "\t'(') - 2)      AS HPC_ASSIGNMENT,\n" +
+            "\tHPC_CREATED_BY_NAME,\n" +
+            "\t'RESOLUTION'   AS RESOLUTION,\n" +
+            "\t'OPENED_BY'    AS OPENED_BY \n" +
+            "FROM\n" +
+            "\tsmprimary.SBPROBSUMMARYTSM1 prob1 \n" +
+            "WHERE\n" +
+            "\tprob1.hpc_assignment IN ( 'ЦСИТ Серверы приложений (00001092)',\n" +
+            "\t'ЦСИТ ОАСП Стандартные платформы (00003984)',\n" +
+            "\t'Сопровождение WAS ОСЦИТУ ДВБ (00001341)',\n" +
+            "\t'ЦИ Запад Стандартные платформы (00011217)',\n" +
+            "\t'ЦИ Центр Стандартные платформы (00011213)',\n" +
+            "\t'ЦСИТ ОАСП Специализированные платформы (00003978)',\n" +
+            "\t'ЦСИТ ОАСП Интеграционные платформы (00003982)',\n" +
+            "\t'ЦИ ОАСП Системы очередей сообщений (00014339)',\n" +
+            "\t'ЦИ ОАСП Шлюзовые решения (00014345)',\n" +
+            "\t'ЦИ ОАСП Технологический стэк ППРБ (00014341)',\n" +
+            "\t'ЦИ Восток Интеграционные платформы (00011221)',\n" +
+            "\t'ЦИ Запад Интеграционные платформы (00011219)',\n" +
+            "\t'ЦИ Центр Интеграционные платформы (00011215)',\n" +
+            "\t'СБТ ДК ОСА Серверы приложений (Щелчков Р.А.) (00010280)',\n" +
+            "\t'Сопровождение Платформы управления контейнерами (00018435)',\n" +
+            "\t'SberInfra УСП Интеграционные платформы (Гоголев К.Ю.) (00019273)')) \n" +
+            "WHERE\n" +
+            "\tOPENED_BY = 'int_zabbix_si' AND OPEN_TIME BETWEEN TO_TIMESTAMP('01.04.2022 8:23:16', 'DD.MM.RRRR HH24:MI:SS') AND TO_TIMESTAMP('17.05.2022 8:23:16', 'DD.MM.RRRR HH24:MI:SS')\n" +
+            "\tGROUP BY \"HPC_ASSIGNMENT\" ORDER BY \"COUNT_INC\" DESC",
+            nativeQuery = true)
+    List<UspIncidentData> findIncCount();
+
+//    List<UspIncidentData> findByServerName(String ServerName);
+//
+//    UspIncidentData findById(long id);
+}
--- src/main/java/ru/sberbank/uspincidentreport/view/Analitics.java
+++ src/main/java/ru/sberbank/uspincidentreport/view/Analitics.java
@@ -1,22 +1,88 @@
 package ru.sberbank.uspincidentreport.view;
 
-import com.vaadin.flow.component.html.Div;
+import com.github.appreciated.apexcharts.ApexCharts;
+import com.github.appreciated.apexcharts.ApexChartsBuilder;
+import com.github.appreciated.apexcharts.config.builder.ChartBuilder;
+import com.github.appreciated.apexcharts.config.builder.LegendBuilder;
+import com.github.appreciated.apexcharts.config.builder.PlotOptionsBuilder;
+import com.github.appreciated.apexcharts.config.builder.ResponsiveBuilder;
+import com.github.appreciated.apexcharts.config.chart.Type;
+import com.github.appreciated.apexcharts.config.legend.Position;
+import com.github.appreciated.apexcharts.config.plotoptions.builder.PieBuilder;
+import com.github.appreciated.apexcharts.config.plotoptions.pie.builder.*;
+import com.github.appreciated.apexcharts.config.responsive.builder.OptionsBuilder;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.html.H4;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
-import org.springframework.beans.factory.annotation.Autowired;
+import ru.sberbank.uspincidentreport.domain.UspIncidentData;
+import ru.sberbank.uspincidentreport.repo.UspIncidentCountRepo;
 import ru.sberbank.uspincidentreport.repo.UspIncidentRepo;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Route(value = "analitics")
-@PageTitle("Аналитика автоинцидентов УСП")
-public class Analitics extends Div {
+@PageTitle("Аналитика автоинцидентов УСП за период")
+public class Analitics extends VerticalLayout {
     private H4 header;
-    @Autowired
-    private UspIncidentRepo repo;
 
     public Analitics() {
-        setText("Hello @Route!");
+        this.header = new H4("Аналитика автоинцидентов УСП за период");
+        setHorizontalComponentAlignment(Alignment.CENTER, header);
+        ApexCharts donutChart = ApexChartsBuilder.get()
+                .withChart(ChartBuilder.get().withType(Type.donut)
+                        .withOffsetX(-300.0)
+                        .build())
+                .withPlotOptions(PlotOptionsBuilder.get().withPie(PieBuilder.get()
+                        .withDonut(DonutBuilder.get()
+                                .withLabels(LabelsBuilder.get()
+                                        .withShow(true)
+                                        .withName(NameBuilder.get().withShow(true).build())
+                                        .withTotal(TotalBuilder.get().withShow(true).withLabel("Всего автоинцидентов за период").build())
+                                        .build())
+                                .build())
+                        .build())
+                        .build())
+                .withLegend(LegendBuilder.get()
+                        .withPosition(Position.right)
+                        .withFontSize("15")
+                        .withOffsetX(-10.0)
+                        .build())
+                .withSeries(44.0, 55.0, 41.0, 17.0, 15.0, 14.0, 65.0)
+                .withLabels("Первый", "ЦИ ОАСП Системы очередей сообщений", "Третий", "Четвертый", "Пятый", "ЦИ ОАСП Системы очередей сообщений", "ЦИ ОАСП Системы очередей сообщений")
+                .withResponsive(ResponsiveBuilder.get()
+                        .withBreakpoint(480.0)
+                        .withOptions(OptionsBuilder.get()
+                                .withLegend(LegendBuilder.get()
+                                        .withPosition(Position.bottom)
+                                        .build())
+                                .build())
+                        .build())
+                .build();
+        donutChart.setWidth("1300");
+        donutChart.setHeight("500");
+        donutChart.setVisible(false);
+        donutChart.setVisible(true);
+
+        //Выбор даты
+
+        DatePicker startDate = new DatePicker("Начало");
+        DatePicker endDate = new DatePicker("Конец");
+        startDate.addValueChangeListener(e -> endDate.setMin(e.getValue()));
+        endDate.addValueChangeListener(e -> startDate.setMax(e.getValue()));
+        VerticalLayout dateLayout = new VerticalLayout(startDate, endDate);
+
+        //Отображение. Добавление компонентов
+        HorizontalLayout horizontalLayout = new HorizontalLayout(donutChart, dateLayout);
+        add(header, horizontalLayout);
+
+
     }
 
 }
+
+
+
